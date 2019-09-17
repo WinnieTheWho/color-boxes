47c84471debebf526f0da5a40d6dc04271578c1e
Winnie Yu <YuWinnieC@gmail.com>
2019-09-17 15:21:33 -0700

    added BoxesDisplay.js functions and css to boxes

--- src/BoxesDisplay.js
+++ src/BoxesDisplay.js
@@ -2,21 +2,57 @@ import React, { Component } from "react";
 import Box from "./Box"
 
 class BoxesDisplay extends Component {
+  static defaultProps = {
+    colors: ["blue", "red", "orange", "peachpuff",
+            "yellow", "green", "purple", "pink"]
+  }
+
   constructor(props){
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.state = {boxesArray: 
+    ["blue","blue","blue","blue",
+    "blue","blue","blue","blue",
+    "blue","blue","blue","blue",
+    "blue","blue","blue","blue"]}
+  }
+
+  getRandomIdx() {
+    let randomIdx = Math.floor(Math.random() * this.state.boxesArray.length);
+    return randomIdx;
+  }
+
+  getRandomColor() {
+    let randomCIdx = Math.floor(Math.random() * this.props.colors.length);
+    return this.props.colors[randomCIdx];
   }
 
   handleClick(evt) {
-    console.log("THIS IS CLICKED");
+    let randomIdx = this.getRandomIdx();
+    let randomColor = this.getRandomColor();
+    this.setState(st => ({
+      boxesArray: st.boxesArray.map((box, i, arr) => {
+      if(i === randomIdx) {
+        return arr[i] = randomColor;
+      } else {
+        return arr[i];
+      }
+      })
+    }))
   }
 
   render() {
+    let multiBox = [];
+    let colors = this.state.boxesArray
+    for(let i=0; i < colors.length; i++) {
+      multiBox.push(<div>
+        <Box color={colors[i]}/>
+        <button onClick={this.handleClick}>CLICK</button>
+        </div>)
+    }
     return (
       <div className="boxdisplay">
-        <Box color="blue"/>
-        <Box color="red"/>
-        <Box color="green"/>
+        {multiBox}
       </div>
     )
   }
